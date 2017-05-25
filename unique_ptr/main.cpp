c2bc35ecf8d23fd8ba9498661ec5ac8a9aced0d2
Rajesh Agrawal <rkagrawal@gmail.com>
2017-05-25 10:35:16 -0400

    make shared and unique with raw pointers cause memory leak

--- unique_ptr/main.cpp
+++ unique_ptr/main.cpp
@@ -18,8 +18,7 @@ int main(int argc, char* argv[] ) {
     up1->print(cout);
     up2->print(cout);
     
-    // The same logic should apply to shared_ptr as well.
-    
+    //The same logic should apply to shared_ptr as well.
     // create a shared ptr from unique_ptr ( move it )..
     shared_ptr<emp> sp1 = move(up1);
 
@@ -31,4 +30,19 @@ int main(int argc, char* argv[] ) {
     // both pointer and * work
     sp1->print(cout);
     (*sp1).print( cout );
+
+    /*
+     *  If you pass make_unique a ptr, that will call a constructor that requires a ptr
+     *  it will create a new copy  of the object, but the passed in pointer will not be deleted 
+     *  and cause a memory leak. as seen in the example of up3
+     *  If emp ( const emp* const ) ctor is not there, it will cause a compilation error
+     *  as make_unique will try to create a new object with ctrol block from emp*
+     *  same logic goes for make_shared as well as can be seem from sp3 below
+     */
+    unique_ptr<emp> up3 = make_unique<emp>(  new emp("Agrawal", "Mahesh", 50 ) ); 
+    up3->print(cout);
+
+    shared_ptr<emp> sp3 = make_shared<emp>( new emp( "xyz", "abc", 99 ) );
+    sp3->print(cout);
+
 }
