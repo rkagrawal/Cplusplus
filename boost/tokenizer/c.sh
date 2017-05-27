e61a838b65f13d722e9003d8d0eca63cdd33eba3
Rajesh Agrawal <rkagrawal@gmail.com>
2017-05-27 11:21:26 -0400

    -I include not necessary

--- boost/tokenizer/c.sh
+++ boost/tokenizer/c.sh
@@ -1,2 +1,3 @@
-INC=usr/local/Cellar/boost/1.64.0_1/include
-clang++ -I$INC -std=c++14 -g -o main.out main.cpp
+INC=/usr/local/Cellar/boost/1.64.0_1/include
+# Do not need -I $INC below as /usr/local/include/boost links to /usr/local/Cellar/boost
+clang++ -std=c++14 -g -o main.out main.cpp
