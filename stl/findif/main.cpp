b84f33a61293af7ee320a716a17ebe2652c7dd38
Rajesh Agrawal <rkagrawal@gmail.com>
2017-06-07 11:43:37 -0400

    copy_if example included

--- stl/findif/main.cpp
+++ stl/findif/main.cpp
@@ -7,7 +7,10 @@
 //
 #include <iostream>
 #include <deque>
+#include <vector>
+#include <iterator>
 #include <algorithm>
+#include <assert.h>
 
 template< typename T>
 class GreaterThanFive {
@@ -19,8 +22,7 @@ public:
 };
 
 
-#include <iostream>
-
+using namespace std;
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
@@ -38,7 +40,18 @@ int main(int argc, const char * argv[]) {
     GreaterThanFive<double> x(5);
     std::deque<double>::iterator iter;
     iter = std::find_if( d.begin(), d.end(), GreaterThanFive<double>(5) );
-    std::cout << *iter << std::endl;
+    int v1 = *iter;
+    cout << "The value of v1 is " << v1 << endl;
+    
+    //using lambda with init capture
+    iter = std::find_if( d.begin(), d.end(), [v=5]( int val ) { return val > 5 ; } );
+    int v2 = *iter;
+    cout << "The value of v2 is " << v2 << endl;
+    assert( v1 == v2 );
 
+
+    vector<double> allMoreThan5;
+    copy_if( d.begin(), d.end(), back_inserter(allMoreThan5), GreaterThanFive<double>(5) );  
+    copy( allMoreThan5.begin(), allMoreThan5.end(), ostream_iterator<double>( cout, " " ) ); cout << "\n"; 
     return 0;
 }
