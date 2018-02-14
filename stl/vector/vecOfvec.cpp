77a3059338d4586f06bc9755747d116db4b80a96
Rajesh Agrawal <rajesh@Rajeshs-iMac.fios-router.home>
2018-02-13 20:57:01 -0500

    ..

--- stl/vector/vecOfvec.cpp
+++ stl/vector/vecOfvec.cpp
@@ -16,4 +16,18 @@ int main( int argc, const char* argv[] ) {
     vs[8][2] = "Rajesh";
     cout << ms.vv[0][0] << endl;
     cout << vs[8][2] << endl;
+
+    vector<string> strvec = { "abc", "efg", "klm" };
+    cout << "The size of strvec is " << strvec.size() << endl;
+
+    vector<vector<string>> xyz(4, vector<string>() );
+   for( int i=0; i< 4; i++ ) 
+	xyz[i].assign( strvec.begin(), strvec.end() );
+
+   
+   for( int i=0; i< 4; i++ ) {
+	cout << "The size is " << xyz[i].size() << endl;
+	for ( int j=0; j<xyz[i].size(); j++ ) 
+		cout << "*" <<xyz[i][j] << endl;
+   }
 }
