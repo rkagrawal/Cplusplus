beb8ca86ddb7ad9551b86ef99366b7e4de805e26
Rajesh Agrawal <rkagrawal@gmail.com>
2020-06-24 18:25:10 +0100

    tuple element example

---
+++ stl/tuple_element/main.cpp
@@ -0,0 +1,37 @@
+/* there is std::tuple_element that gets the type of the 1th element of a tuple */
+/* This exercise is to write one of my own to see if I understand */
+
+#include<tuple>
+#include<iostream>
+
+namespace rka {
+	
+	template< std::size_t I, class T>
+	struct tuple_element;
+
+	template<std::size_t I, class Head, class... Tail > 
+	struct tuple_element<I, std::tuple<Head, Tail...>> : tuple_element<I-1, std::tuple<Tail...>> {};
+
+	template< class Head, class... Tail>
+	struct tuple_element<0, std::tuple<Head, Tail...>> {
+		typedef Head type;
+	};
+}
+
+template <class... Args>
+struct type_list
+{
+   template <std::size_t N>
+   using type = typename rka::tuple_element<N, std::tuple<Args...>>::type;
+};
+ 
+int main()
+{
+   std::cout << std::boolalpha;
+   type_list<int, char, bool>::type<2> x = true;
+   type_list<int, char, bool>::type<0> y = 25;
+   type_list<int, char, bool, std::string>::type<3> z = std::string("mine");
+   std::cout << x << '\n';
+   std::cout << y << '\n';
+   std::cout << z << '\n';
+}
