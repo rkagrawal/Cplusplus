b0e4677bbe41c385c82cceb0fa994293767a5e20
Rajesh Agrawal <rkagrawal@gmail.com>
2020-07-28 21:40:42 +0100

    more ex

--- stl/accumulate/c.sh
+++ stl/accumulate/c.sh
@@ -1,2 +1,4 @@
-clang++ -std=c++14 -g -o youngest.out youngest.cpp
-clang++ -std=c++14 -g -o acc.out acc.cpp
+#g++ -std=c++14 -g -o youngest.out youngest.cpp
+#g++ -std=c++14 -g -o acc.out acc.cpp
+g++ -std=c++14 -g -o main.out main.cpp
+
---
+++ stl/accumulate/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <map>
+#include <numeric>
+
+struct TQ {
+	TQ( int x ) : totalQty(x) {}
+	int totalQty ;
+
+	int getTotalQty() { return totalQty; }
+};
+
+
+int main() {
+
+	std::map< int, TQ > m { {1, 30},
+		{2, -39},
+		{3, 5} };
+
+
+   int t = std::accumulate( m.begin(), m.end(), 0,
+						[]( int totQty, std::pair<int, TQ> p ) {
+							totQty += abs(p.second.getTotalQty());
+                            return totQty;
+						});
+
+	std::cout << "The total qty is " << t << std::endl;
+}
