05f52ce44fe1b54e2ea87d5e8f4bb38871ad6252
Rajesh <rkagrawal@gmail.com>
2022-04-30 16:58:46 -0400

    use onlinux

--- gtest/c.sh
+++ gtest/c.sh
@@ -1,8 +1,8 @@
-INC=${HOME}/googletest/googletest/include
-LIB=${HOME}/googletest/build/lib
+INC=${HOME}/google/googletest/googletest/include
+LIB=${HOME}/google/googletest/build/lib
 
 g++ -std=gnu++14 -I $INC -c emp.cpp
 g++ -std=gnu++14 -I $INC -c fac.cpp
 g++ -std=gnu++14 -I $INC -c emptest.cpp
-g++ -std=gnu++14 -I $INC -o emptest.out emptest.o emp.o fac.o -L$LIB  -lgtest -lgtest_main -lgmock_main
+g++ -std=gnu++14 -I $INC -o emptest.out emptest.o emp.o fac.o -L$LIB  -lgtest -lgtest_main -lgmock_main -lpthread
 
